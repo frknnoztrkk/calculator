b51647dc5c645394b3077f0d3c32ead8824fc763
Furkan Öztürk <furkan25ozturk@gmail.com>
2024-03-09 00:13:19 +0300

    useState_useReduce

---
+++ Calculator/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Calculator from './Components/Calculator'
+import CalculatorUseReducer from './Components/CalculattorUseReducer'
+
+
+function App() {
+  
+
+  return (
+   <div className='App'>
+    < Calculator />
+    < CalculatorUseReducer />
+   </div>
+  )
+}
+
+export default App
---
+++ Calculator/src/Components/CalculattorUseReducer.jsx
@@ -0,0 +1,78 @@
+import { useReducer } from "react";
+import "./Calculator.css";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "SET_NUMBER1":
+      return { ...state, number1: action.payload };
+    case "SET_NUMBER2":
+      return { ...state, number2: action.payload };
+    case "SET_RESULT":
+      return { ...state, result: action.payload };
+    default:
+      throw new Error("Invalid action type!");
+  }
+}
+
+const initialState = {
+  number1: 0,
+  number2: 0,
+  result: 0,
+};
+
+const CalculatorUseReducer = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  console.log(state);
+  const handleNumber1Change = (e) => {
+    dispatch({ type: "SET_NUMBER1", payload: Number(e.target.value) });
+  };
+
+  const handleNumber2Change = (e) => {
+    dispatch({ type: "SET_NUMBER2", payload: Number(e.target.value) });
+  };
+  const handleAddition = () => {
+    dispatch({ type: "SET_RESULT", payload: state.number1 + state.number2 });
+    dispatch(number1 + number2);
+  };
+
+  return (
+    <div className="calculator-container">
+      <h1>useReducer</h1>
+      <div className="form-container">
+        <div className="form-group">
+          <label className="form-label" htmlFor="number1">
+            Number1
+          </label>
+          <input
+            className="form-input"
+            type="number"
+            id="number1"
+            onChange={handleNumber1Change}
+          />
+        </div>
+        <div className="form-group">
+          <label
+            className="form-label"
+            htmlFor="number2"
+            onChange={handleNumber2Change}
+          >
+            Number2
+          </label>
+          <input className="form-input" type="number" id="number2" />
+        </div>
+        <button className="form-button" onClick={handleAddition}>
+          Add
+        </button>
+        <div className="form-group">
+          <label className="form-label" htmlFor="result">
+            Result
+          </label>
+          <input className="form-input" type="number" id="result" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CalculatorUseReducer;
