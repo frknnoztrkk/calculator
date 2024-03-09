c823eb9ce47f408de8d69c946e0de90205327971
Furkan Öztürk <furkan25ozturk@gmail.com>
2024-03-09 11:17:28 +0300

    Debug

--- Calculator/src/Components/CalculattorUseReducer.jsx
+++ Calculator/src/Components/CalculattorUseReducer.jsx
@@ -1,29 +1,12 @@
 import { useReducer } from "react";
 import "./Calculator.css";
+import { initialState, reducer } from "../reducers/Calculator";
 
-function reducer(state, action) {
-  switch (action.type) {
-    case "SET_NUMBER1":
-      return { ...state, number1: action.payload };
-    case "SET_NUMBER2":
-      return { ...state, number2: action.payload };
-    case "SET_RESULT":
-      return { ...state, result: action.payload };
-    default:
-      throw new Error("Invalid action type!");
-  }
-}
 
-const initialState = {
-  number1: 0,
-  number2: 0,
-  result: 0,
-};
 
 const CalculatorUseReducer = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  console.log(state);
   const handleNumber1Change = (e) => {
     dispatch({ type: "SET_NUMBER1", payload: Number(e.target.value) });
   };
@@ -31,9 +14,9 @@ const CalculatorUseReducer = () => {
   const handleNumber2Change = (e) => {
     dispatch({ type: "SET_NUMBER2", payload: Number(e.target.value) });
   };
+
   const handleAddition = () => {
     dispatch({ type: "SET_RESULT", payload: state.number1 + state.number2 });
-    dispatch(number1 + number2);
   };
 
   return (
@@ -49,26 +32,39 @@ const CalculatorUseReducer = () => {
             type="number"
             id="number1"
             onChange={handleNumber1Change}
+            value={state.number1}
           />
         </div>
         <div className="form-group">
-          <label
-            className="form-label"
-            htmlFor="number2"
-            onChange={handleNumber2Change}
-          >
+          <label className="form-label" htmlFor="number2">
             Number2
           </label>
-          <input className="form-input" type="number" id="number2" />
+          <input
+            className="form-input"
+            type="number"
+            id="number2"
+            onChange={handleNumber2Change}
+            value={state.number2}
+          />
         </div>
-        <button className="form-button" onClick={handleAddition}>
+
+        <button
+          className="form-button"
+          onClick={handleAddition}>
           Add
         </button>
+
         <div className="form-group">
           <label className="form-label" htmlFor="result">
             Result
           </label>
-          <input className="form-input" type="number" id="result" />
+          <input
+            className="form-input"
+            type="number"
+            id="result"
+            value={state.result}
+            readOnly
+          />
         </div>
       </div>
     </div>
